2a8b33cc3615ff479c78272a4d52352c542ae4dd
Ziyad AlGhunaim <ZiyadKAG@gmail.com>
2020-05-28 21:53:31 -0500

    idk

---
+++ Source/TimeSink/CharacterAttributeSet.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterAttributeSet.h"
+
+UCharacterAttributeSet::UCharacterAttributeSet() : Health(100.f)
+{
+}
